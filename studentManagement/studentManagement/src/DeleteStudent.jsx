68b7d7618f5d8a49530fd7289cde53670328a341
Radhika Jha <119055548+iamradhikajha@users.noreply.github.com>
2025-12-03 23:24:50 +0530

    Delete studentManagement/studentManagement directory

--- studentManagement/studentManagement/src/DeleteStudent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function DeleteStudent() {
-  return <div>This is DeleteStudent</div>;
-}
-
-export default DeleteStudent;
--- studentManagement/studentManagement/src/UpdateStudent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function UpdateStudent() {
-  return <div>This is UpdateStudent</div>;
-}
-
-export default UpdateStudent;
